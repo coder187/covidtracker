a0551db862e79e56b5ea2737004661d103a3a353
Jonathan Kelly <coder187@hotmail.com>
2021-05-17 21:33:16 +0100

    add function to check for duplicate county records

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,13 +42,76 @@ function GetCovidStats() {
     });
 }
 
-function GetCountyStats() {
-
+function GetCountyStats2() {
+    //stub  - remove later.
     let ul = document.getElementById("county-list");
     let county = ["Limerick", "Tipp", "Cork", "Dublin", "Galway", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"];
     for (var i = 1; i < county.length; i++) {
         let li = document.createElement("li");
         li.innerHTML = county[i] + " :loading...";
         ul.appendChild(li);
+    }
+}
+
+function GetCountyStats() {
+    //get the total cases per county.
+ 
+
+    return new Promise(function (resolve, reject) {
+        let url = "https://services1.arcgis.com/eNO7HHeQ3rUcBllm/ArcGIS/rest/services/Covid19CountyStatisticsHPSCIrelandOpenData/FeatureServer/0/query?"
+
+        require(["esri/request"],
+            function (esriRequest) {
+                // Define the 'options' for the request
+                let options = {
+                    query: {
+                        f: "json",
+                        where: "1=1", //seems to need a where clause to return anything - rertturn all records.
+                        outFields: ["CountyName", "ConfirmedCovidCases", "PopulationProportionCovidCases", "ConfirmedCovidDeaths", "ConfirmedCovidRecovered", "TimeStampDate"],
+                        returnGeometry: false
+                    },
+                    responseType: "json",
+                };
+               
+                //console.log("call request");
+
+                esriRequest(url, options).then(function (response) {
+                    //console.log(response);
+                    let myFeatures = response.data.features;
+                    let stats_Array = [];
+                    let stats = {};
+
+                    for (var i = 0; i < (myFeatures.length) / 2; i++) {//end point returns the data duplicted.
+                        if (!CountyExists(myFeatures[i].attributes.CountyName,stats_Array)) {
+                            stats = {
+                                County: myFeatures[i].attributes.CountyName,
+                                Confirmed: myFeatures[i].attributes.ConfirmedCovidCases,
+                                Proportion: myFeatures[i].attributes.PopulationProportionCovidCases,
+                                ConfirmedDeaths: myFeatures[i].attributes.ConfirmedCovidDeaths,//currently null
+                                ConfirmedRecovered: myFeatures[i].attributes.ConfirmedCovidRecovered,//currently null
+                                TimeStamp: new Date(myFeatures[i].attributes.TimeStampDate)
+                            }
+                 
+                            stats_Array.push(stats);
+                        }
+                    }
+                    
+                    resolve(stats_Array); //return the stats array of objects
+                });
+            });
+
+        //resolve(stats); not sure why stats cabnt be read here.
+    });
+}
+
+//the end point returns seemingly duplicate data but we only need one copy.
+function CountyExists(county, stats_array) {
+    for (let i = 0; i < stats_array.length; i++) {
+        if (stats_array[i].county === county) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
